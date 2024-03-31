47dd9042a32f26c0a54c84de3e0f00b7005df1e9
haiduong183724 <haiduongtd2k@gmail.com>
2024-04-01 06:17:41 +0700

    fix error in some editor

--- script/CheckAndRestartDockerContainers.ps1
+++ script/CheckAndRestartDockerContainers.ps1
@@ -1,22 +1,28 @@
 # get list all containers
-$containers = docker ps --format "{{.Names}}"
-$count = $containers.Count
+$Containers = docker ps --format "{{.Names}}"
+$Containers = ($Containers -replace 'failed to get console mode for stdout: The handle is invalid.', '') -split '\r?\n' | Where-Object { $_ }
+$count = $Containers.Count
+Write-Host "$count containers is running."
 if($count -lt 2){
 	Write-Host "Only $count containers is running. restart all."
 	Write-Output "Restarting $container due to 0% CPU usage..."
 	.\start_io_worker.bat
 }
 else{
-	foreach ($container in $containers) {
-		$stats = docker stats --no-stream --format "{{.CPUPerc}}" $container
-		$cpuUsage = $stats -replace "%", "" -as [float]
+	foreach ($c in $Containers) {
+		$stats = docker stats --no-stream --format "{{.CPUPerc}}" $c
+		Write-Output "stat:$stats"
+		$stats = ($stats -replace 'failed to get console mode for stdout: The handle is invalid.', '').Trim() -split '\r?\n' | Where-Object { $_ }[0]
+		$cpuUsage = $stats.Trim().TrimEnd('%').TrimStart() -as [float]
+		Write-Output "stat:$stats"
+		Write-Output "cpu usage: $cpuUsage"
 		if ($cpuUsage -eq 0) {
-			Write-Output "Restarting $container due to 0% CPU usage..."
+			Write-Output "Restarting $c due to 0% CPU usage..."
 			.\start_io_worker.bat
 			break
 		}
 		else {
-			Write-Output "$container is running with CPU usage at $cpuUsage%"
+			Write-Output "$c is running with CPU usage at $cpuUsage %"
 		}
 	}
 }
