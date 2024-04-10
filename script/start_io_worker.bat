ed24602ac0e3a9e709e60dc8fdcd0749f0431e2b
haiduong183724 <63958508+haiduong183724@users.noreply.github.com>
2024-04-10 20:32:03 +0700

    Update start_io_worker.bat

--- script/start_io_worker.bat
+++ script/start_io_worker.bat
@@ -17,6 +17,6 @@ for /f "tokens=1" %%i in ('docker container ls -q') do (
     echo Removed container %%i
 )
 
-docker run -d -v /var/run/docker.sock:/var/run/docker.sock -e DEVICE_NAME="remote-gpu-node-03" -e DEVICE_ID=bdb2ac4d-35a2-4158-9cb2-9f5d15aee971 -e USER_ID=dcee5b04-9ced-4ff0-90fa-c845089865b9 -e OPERATING_SYSTEM="Windows" -e USEGPUS=true --pull always ionetcontainers/io-launch:v0.1
+docker run -d -v /var/run/docker.sock:/var/run/docker.sock -e DEVICE_NAME="devicename" -e DEVICE_ID=device_id -e USER_ID=user_id -e OPERATING_SYSTEM="Windows" -e USEGPUS=true --pull always ionetcontainers/io-launch:v0.1
 
 
